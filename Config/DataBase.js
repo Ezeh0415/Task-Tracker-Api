5fdbe86ecad8fd80f6471858ba5e9ab611a5a333
Ezeh Godwin <EzehGodwin3942@gmail.com>
2025-12-05 18:57:51 +0100

    addded tables

--- Config/DataBase.js
+++ Config/DataBase.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const pkg = require("pg");
 const { drizzle } = require("drizzle-orm/node-postgres");
+const { TasksTable, UsersTable } = require("../Model/Tables");
 const { Pool } = pkg;
 
 // Build PostgreSQL connection URL from environment variables
@@ -24,29 +25,8 @@ const db = drizzle({ client: pool });
   try {
     await pool.query("select 1"); // simple test query
     console.log("Database connection successful");
-    await pool.query(`
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-`); // user table creation
-    await pool.query(`
-CREATE TABLE IF NOT EXISTS tasks (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  description VARCHAR(255) NOT NULL,
-  due_date VARCHAR(50) NOT NULL,
-  priority VARCHAR(255) NOT NULL,
-  status VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-`); // task table creation
+    await pool.query(UsersTable); // user table creation
+    await pool.query(TasksTable); // task table creation
   } catch (err) {
     console.error("Database connection failed:", err);
   }
---
+++ Model/Tables.js
@@ -0,0 +1,24 @@
+const UsersTable = `CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);`;
+
+const TasksTable = `
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  description VARCHAR(255) NOT NULL,
+  due_date VARCHAR(50) NOT NULL,
+  priority VARCHAR(255) NOT NULL,
+  status VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+`;
+
+module.exports = { UsersTable, TasksTable };
